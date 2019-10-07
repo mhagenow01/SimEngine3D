1f8cf98be6aef4fedc6ee3ceef465fbf024ec4fc
ENGR\mhagenow <mhagenow@wisc.edu>
2019-10-07 08:23:45 -0500

    starting code for DP1

--- GCons/DP1.py
+++ GCons/DP1.py
@@ -1,18 +1,23 @@
 """ Defines the class of related functions for the dot product one constraint
 """
 
+import numpy as np
+from Utilities.kinematic_identities import A_from_p
 
 class DP1:
-    def __init__(self, test1,test2):
-        self.test1=test1
+    def __init__(self,i,a_bar_i,j,a_bar_j,f_t):
+        self.i = i
+        self.a_bar_i=a_bar_i
+        self.j=j
+        self.a_bar_j = a_bar_j
+        self.f_t=f_t
 
-    def phi:
-
+    def phi(self,i,a_bar_i,j,a_bar_j,f_t):
+        return a_bar_i.transpose() @ A_from_p(i.p).transpose() @ A_from_p(j.p) @ a_bar_j -f_t
     def nu:
 
     def gamma:
 
     def phi_r:
-
 
     def phi_p:
--- Utilities/kinematic_identities.py
+++ Utilities/kinematic_identities.py
@@ -1,2 +1,14 @@
 """ Kinematic identities
 """
+
+import numpy as np
+
+def A_from_p(p):
+    e0=p[0]
+    e=np.array(p[1:]).reshape((3, 1))
+    return (2*e0-1)*np.eye(3)+2*(np.matmul(e, e.reshape((1, 3)))+e0*a_tilde(e))
+
+def a_tilde(a):
+    return np.array([0.,-a[2], a[1]],
+                    [a[2], 0., -a[0]],
+                    [-a[1], a[0], 0.]);
