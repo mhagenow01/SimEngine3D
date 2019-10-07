adc213957caaa8ad150101c207c0fbae13279249
ENGR\mhagenow <mhagenow@wisc.edu>
2019-10-07 07:51:03 -0500

    added some directories for organization

---
+++ GCons/DP1.py
@@ -0,0 +1,18 @@
+""" Defines the class of related functions for the dot product one constraint
+"""
+
+
+class DP1:
+    def __init__(self, test1,test2):
+        self.test1=test1
+
+    def phi:
+
+    def nu:
+
+    def gamma:
+
+    def phi_r:
+
+
+    def phi_p:
