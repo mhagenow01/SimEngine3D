09873714680f2e8f61cc367de0525a90b984d45f
mhagenow01 <36891409+mhagenow01@users.noreply.github.com>
2019-10-12 14:40:19 -0500

    updated comments

--- simEngine3DA6P2.py
+++ simEngine3DA6P2.py
@@ -10,8 +10,7 @@ from GCons.DP1 import DP1
 
 def setUpPendulum():
 
-    # Define the two bodies
-
+    ###### Define the two bodies ################
     # Body j is going to be the ground and as such doesn't have any generalized coordinates
     j = RigidBody()  # defaults are all zero
     s_bar_j_q = np.array([0., 0., 0.]).reshape((3,1))
@@ -35,20 +34,19 @@ def setUpPendulum():
     # Driving constraint will use y' and y to define trig function
     d_bar_j = np.array([0., -1., 0.]).reshape((3,1))
 
-    # Define the geometric constraints
+    ###### Define the geometric constraints ################
     RJ = Revolute(i, s_bar_i_q, a_bar_i, b_bar_i, j, s_bar_j_q, c_bar_j, j_ground=True)
 
-    # Define the driving constraints for time zero
+    ###### Define the driving constraints for time zero ################
     # Function: theta = pi/4 * cos(2t)
     t = 0.
     f = np.sin(np.pi / 4 * np.cos(2 * t))
-    f = np.sin(np.pi/4)
     f_dot = -np.cos(np.pi/4 * np.cos(2*t)) * np.pi/2 * np.sin(2*t)
     f_ddot = np.sin(np.pi/4 * np.cos(2*t)) * np.power(np.pi,2)/4 * np.power(np.sin(2*t),2)- \
              -np.cos(np.pi / 4 * np.cos(2 * t)) * np.pi * np.cos(2*t)
     dp1 = DP1(i, a_bar_i, j, d_bar_j, f, f_dot, f_ddot, j_ground=True)
 
-
+    ###### Print the Desired Initial Properties ########################
     PHI = np.concatenate((RJ.phi(), dp1.phi().reshape((1,1))),axis=0)
     print("PHI:")
     print(PHI)
